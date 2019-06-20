6ba23c3f6c41649417818823332842fc4eaa1ba5
Joe McGrath <joseph.mcgrath@outlook.com>
2019-06-20 22:15:18 +0100

    Re-worked blog metadata handling.

--- generation/build_site.py
+++ generation/build_site.py
@@ -8,32 +8,39 @@ import datetime
 
 
 class simple_markdown:
+    """Converts a markdown file into html with attached metadata."""
 
-    markdown_extensions = ["fenced_code", 'meta']
+    markdown_extensions = ["fenced_code", "meta"]
 
-    def __init__(self, file_path):
+    def __init__(self, src_path):
+        self.parse_markdown(src_path)
+
+    def parse_markdown(self, src_path):
+        """
+        Converts a markdown file to html (results stored on the simple_markdown
+        object rather than returned).
+        """
         markdown_parser = markdown.Markdown(extensions=self.markdown_extensions)
 
-        with open(file_path, "r") as f:
+        with open(src_path, "r") as f:
             lines_in = f.readlines()
 
-        self.file_path = file_path
+        self.file_path = src_path
         self.markdown = "".join([x for x in lines_in if not re.match(r"^\@", x)])
         self.html = markdown_parser.convert(self.markdown)
-        self.meta = self.consolidate_meta(markdown_parser.Meta)
-        self.title = self.meta.get("title")
+        self.meta = markdown_parser.Meta
 
-    def consolidate_meta(self, meta_seq):
-        output = {
-            key: ",".join(meta_seq[key])
-            for key in meta_seq
-        }
-        self.finished = output.get("finished") == "True" or False
+        if "finished" in self.meta:
+                self.finished = self.meta.get("finished")[0] == "True"
+        else:
+            False
+        self.title = self.meta.get("title")[0]
+
+    def metadata(self):
+        """Consolidates metadata entries to strings."""
+        output = {key: ",".join(self.meta[key]) for key in self.meta}
 
         return output
-
-    def strip_whitespace(self, text):
-        return re.sub(r"(^\s+)|(\s+$)", "", text)
 
 
 class site_press:
@@ -45,6 +52,7 @@ class site_press:
         self.env = jinja2.Environment(loader=file_loader)
 
     def make_index(self):
+        """Generates the site index / home page."""
         template = self.env.get_template("page.html")
         body = self.env.get_template("index.html")
 
@@ -67,44 +75,47 @@ class site_press:
             f.writelines(index_page)
 
     def webify(self, string):
+        """Converts a string into one suitable for a url."""
         string = string.lower()
         string = string.replace("_", "-")
         string = re.sub(r"\s|\_", "-", string)
         return string
 
-    def blog_path(self, path, dir_out="blog"):
+    def blog_path(self, src_path, dir_out="blog"):
         """Alters a markdown file path to be under the blog directory (on disc)."""
-        file_name = self.blog_name(path)
+        file_name = self.blog_name(src_path)
         file_name = os.path.splitext(file_name)[0] + ".html"
 
         file_out = os.path.join(self.root_dir, dir_out, file_name)
         return file_out
 
-    def blog_name(self, path):
-        file_name = self.webify(os.path.split(path)[-1])
+    def blog_name(self, src_path):
+        """Extracts a blog name from a file path."""
+        file_name = self.webify(os.path.split(src_path)[-1])
         file_name = os.path.splitext(file_name)[0]
         return file_name
 
-    def render_blog(self, file_in):
-        file_out = self.blog_path(file_in)
+    def render_blog(self, src_file):
+        """Renders a single markdown blog to a html file."""
+        file_out = self.blog_path(src_file)
         template = self.env.get_template("page.html")
 
-        blog_in = simple_markdown(file_in)
-
-        page_out = template.render(
-            title=blog_in.title,
-            style=self.css_style,
-            body=blog_in.html,
-            metadata=blog_in.meta,
-        )
+        blog_in = simple_markdown(src_file)
 
         if blog_in.finished:
+            page_out = template.render(
+                title=blog_in.title,
+                style=self.css_style,
+                body=blog_in.html,
+                metadata=blog_in.metadata(),
+            )
             with open(file_out, "w") as f:
                 f.writelines(page_out)
 
         return blog_in
 
     def render_all_blogs(self):
+        """Renders all finished blogs and create the blog list."""
         page = self.env.get_template("page.html")
         b_list = self.env.get_template("blog_list.html")
         search_dir = os.path.join(self.root_dir, "markdown")
@@ -112,23 +123,25 @@ class site_press:
         all_files = [x for x in all_files if re.match(r".*\.md$", x)]
         all_files = [os.path.join(search_dir, x) for x in all_files]
 
-        blog_list = []
+        self.blog_list = []
         for this_blog in all_files:
             temp = self.render_blog(this_blog)
             if temp.finished:
-                blog_list.append(temp)
+                self.blog_list.append(temp)
 
-        for this_blog in blog_list:
+        for this_blog in self.blog_list:
             this_blog.url = self.blog_name(this_blog.file_path)
 
         page_out = page.render(
-            title="Blogs", style=self.css_style, body=b_list.render(blogs=blog_list)
+            title="Blogs",
+            style=self.css_style,
+            body=b_list.render(blogs=self.blog_list),
         )
 
         with open(self.blog_path("blogs"), "w") as f:
             f.writelines(page_out)
 
 
-test = site_press(os.path.dirname(os.getcwd()), "/main.css")
-test.make_index()
-test.render_all_blogs()
+site = site_press(os.path.dirname(os.getcwd()), "/main.css")
+site.make_index()
+site.render_all_blogs()
